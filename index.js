df1fe049d00d84a9fa010125de0903f1b0e7ff86
Espen Klem <espen.klem@gmail.com>
2017-02-28 09:00:52 -0500

    All supergroups added

--- index.js
+++ index.js
@@ -1,23 +1,30 @@
 const vinmonopolet = require('vinmonopolet')
 const fs = require('fs');
-const filter = {category:'all',regex:'.'}
-//const filter = {category:'Øl',regex:'øl|hveteøl|surøl|ale|porter|stout|klosterstil|bitter|spesial|lager|red\/amber|barley|mjød'}
-//const filter = {category:'Hvitvin',regex:'hvitvin'}
+//const filter = {category:'all',regex:'.'}
 //const filter = {category:'Rødvin',regex:'rødvin'}
+//const filter = {category:'Hvitvin',regex:'hvitvin'}
+//const filter = {category:'Brennevin',regex:'whisky|druebrennevin|likør|akevitt|vodka|fruktbrennevin|rom|gin|bitter|genever'}
+//const filter = {category:'Øl',regex:'øl|hveteøl|surøl|ale|porter|stout|klosterstil|bitter|spesial|lager|red\/amber|barley|mjød'}
+//const filter = {category:'Musserende vin',regex:'musserende|champagne'}
+//const filter = {category:'Sterkvin',regex:'portvin|sterkvin|sherry|vermut|madeira'}
+//const filter = {category:'Perlende vin',regex:'perlende'}
+//const filter = {category:'Sider',regex:'sider'}
+//const filter = {category:'Aromatisert vin',regex:'aromatisert'}
+//const filter = {category:'Fruktvin',regex:'fruktvin'}
+const filter = {category:'Alkoholfritt',regex:'alkoholfri'}
 const deleteElements = ['url','barcode','images','method','storeable','foodPairing','eco','gluten','kosher','fairTrade','bioDynamic','mainProducer','distributor','distributorId','wholesaler','categories','storeCategory','mainCategory','mainSubCategory','subDistrict','buyable','deliveryTime','nrOfUsage','availableForPickup','averageRating','stock','status','ageLimit','expiredDate','purchasable','newProduct']
 const convertToArray = ['productType','mainCountry']
 const setToId = 'code'
 const filename = 'products-' + filter.category +'.str'
 const writeStream = fs.createWriteStream(filename)
 var i = 0
+regex = new RegExp(filter.regex, 'gi')
 
 vinmonopolet
   .stream.getProducts()
   .on('data', function(product) {
     // Filter on productType
     var productType = product.productType
-    regex = new RegExp(filter.regex, 'gi')
-    console.log(regex)
     if (productType.match(regex)) {
       console.log('Saving product: ' + product.code + ' ' + product.productType)
       // Remove unwanted elements in object
